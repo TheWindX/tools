bc542020df786855aa1b6aaa4e54241c60f81180
xf <lxf0525@gmail.com`>
2015-04-25 20:53:23 +0800

    InputEvents.cs

---
+++ InputEvents/InputEvents.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Runtime.InteropServices;
+
+
+public class InputEvents
+{
+
+    [DllImport("user32")]
+    public static extern short GetKeyState(int nVirtKey);
+    public static bool GetKeyPressState(int vk)
+    {
+        int ks = GetKeyState(vk) & 0x8000;
+        if (ks > 0)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    [DllImport("User32")]
+    public extern static void mouse_event(int dwFlags, int dx, int dy, int dwData, IntPtr dwExtraInfo);
+
+    [DllImport("User32")]
+    public extern static void SetCursorPos(int x, int y);
+
+    [DllImport("User32")]
+    public extern static bool GetCursorPos(out POINT p);
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct POINT
+    {
+        public int X;
+        public int Y;
+    }
+
+    public enum MouseEventFlags
+    {
+        Move = 0x0001,
+        LeftDown = 0x0002,
+        LeftUp = 0x0004,
+        RightDown = 0x0008,
+        RightUp = 0x0010,
+        MiddleDown = 0x0020,
+        MiddleUp = 0x0040,
+        Wheel = 0x0800,
+        Absolute = 0x8000
+    }
+
+    public static void click()
+    {
+        POINT p = new POINT();
+        GetCursorPos(out p);
+        try
+        {
+            SetCursorPos(p.X, p.Y);
+            mouse_event((int)(MouseEventFlags.LeftDown | MouseEventFlags.Absolute), 0, 0, 0, IntPtr.Zero);
+            mouse_event((int)(MouseEventFlags.LeftUp | MouseEventFlags.Absolute), 0, 0, 0, IntPtr.Zero);
+        }
+        finally
+        {
+            SetCursorPos(p.X, p.Y);
+        }
+    }
+}
---
+++ InputEvents/Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using ns_YAUtils;
+
+namespace ns_YAMini
+{
+    class TimerProvide : Singleton<TimerProvide>
+    {
+        uint mTime;
+        public void updateTimer()
+        {
+            var tmp = (uint)(System.DateTime.Now.Ticks / 10000 << 4 >> 4);
+            if (tmp > mTime)
+                mTime = tmp;
+        }
+
+        public uint nowTimer()
+        {
+            return mTime;
+        }
+    };
+
+    class TLoop : Singleton<TLoop>
+    {
+        public void update()
+        {
+            if (evtUpdate != null)
+                evtUpdate();
+        }
+
+        public bool exit = false;
+
+        public Action evtUpdate;
+    };
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            initAll();
+            while (TLoop.Instance.exit == false)
+            {
+                TLoop.Instance.update();
+            }
+        }
+
+        static void initAll()
+        {
+            init();
+            test1();
+        }
+
+        static void init()
+        {
+            var repl = ns_YAUtils.CSRepl.Instance;
+            repl.start();
+
+            TimerProvide.Instance.updateTimer();
+            ns_YAUtils.TimerManager.Init(TimerProvide.Instance.nowTimer);
+            TLoop.Instance.evtUpdate += TimerProvide.Instance.updateTimer;
+            TLoop.Instance.evtUpdate += ns_YAUtils.TimerManager.tickAll;
+            TLoop.Instance.evtUpdate += repl.runOnce;
+        }
+
+        static bool toggle = false;
+
+
+        static int clickEvent = -1;
+
+        static uint timeInterval = 500;
+
+
+        static void upInterval()
+        {
+            timeInterval = (uint)(timeInterval / 1.1);
+            stopAutoClick();
+            autoClick();
+        }
+
+        static void downInterval()
+        {
+            timeInterval = (uint)(timeInterval * 1.1); 
+            stopAutoClick();
+            autoClick();
+        }
+
+        static void autoClick()
+        {
+            if (clickEvent != -1)
+                ns_YAUtils.TimerManager.get().clearTimer(clickEvent);
+            clickEvent = ns_YAUtils.TimerManager.get().setInterval((i, t) =>
+            {
+                InputEvents.click();
+            }, timeInterval, null,
+            4211111111);
+            toggle = false;
+        }
+        static void stopAutoClick()
+        {
+            if (clickEvent != -1)
+                ns_YAUtils.TimerManager.get().clearTimer(clickEvent);
+            toggle = true;
+        }
+
+
+
+        static void test1()
+        {
+
+            ns_YAUtils.TimerManager.get().setInterval((i, t) =>
+            {
+                if (InputEvents.GetKeyPressState((int)VirtualKeyStates.VK_LCONTROL))
+                {
+                    autoClick();
+                }
+                else if (InputEvents.GetKeyPressState((int)VirtualKeyStates.VK_RETURN))
+                {
+                    stopAutoClick();
+                }
+                else if (InputEvents.GetKeyPressState((int)VirtualKeyStates.VK_UP))
+                {
+                    upInterval();
+                }
+                else if (InputEvents.GetKeyPressState((int)VirtualKeyStates.VK_DOWN))
+                {
+                    downInterval();
+                }
+            }, 100,null, 4211111111);
+        }
+    }
+}
+
