d2b64f5304292cfa41be5b1dbced34e8dfd5275c
xf <lxf0525@gmail.com>
2015-09-20 00:22:17 +0800

    more

--- Touch/MainWindow.xaml.cs
+++ Touch/MainWindow.xaml.cs
@@ -20,9 +20,49 @@ namespace Touch
     /// </summary>
     public partial class MainWindow : Window
     {
+        enum e_createLineSt
+        {
+            e_init,
+            e_pickOnePoint,
+            e_pickSecPoint,
+        }
+        e_createLineSt pickSt = e_createLineSt.e_init;
+        CLink curLink = null;
         public MainWindow()
         {
             InitializeComponent();
+
+            m_node1.evtOnRightDown += pt =>
+            {
+                if(pickSt == e_createLineSt.e_init)
+                {
+                    pickSt = e_createLineSt.e_pickOnePoint;
+                    curLink = new CLink();
+                    curLink.IsEnabled = false;
+                    curLink.IsHitTestVisible = false;
+                    m_panel.Children.Add(curLink);
+                    curLink.start = pt;
+                }
+            };
+
+            m_node2.evtOnLeftUp += pt =>
+            {
+                if (pickSt == e_createLineSt.e_pickOnePoint)
+                { 
+                    pickSt = e_createLineSt.e_init;
+                    curLink.end = pt;
+                }
+            };
+
+        }
+
+        private void m_panel_PreviewMouseMove(object sender, MouseEventArgs e)
+        {
+            if (pickSt == e_createLineSt.e_pickOnePoint)
+            {
+                curLink.end = e.GetPosition(this);
+                this.UpdateLayout();
+            }
         }
     }
 }
--- Touch/viewer/cnode.xaml.cs
+++ Touch/viewer/cnode.xaml.cs
@@ -40,8 +40,17 @@ namespace Touch
         Point mStartPoint;
         Point mStartMousePoint;
         bool drag = false;
+
+        public System.Action<Point> evtOnLeftUp;
+        public System.Action<Point> evtOnRightDown;
+
         private void UserControl_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
+            if(Keyboard.IsKeyDown(Key.LeftCtrl))
+            {
+                return;
+            }
+            
             mStartPoint.X = Margin.Left;
             mStartPoint.Y = Margin.Top;
             mStartMousePoint.X = Mouse.GetPosition(this.Parent as IInputElement).X;
@@ -70,7 +79,33 @@ namespace Touch
         private void UserControl_PreviewMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
         {
             drag = false;
-            this.ReleaseMouseCapture();
+            if(this.IsMouseCaptured)
+                this.ReleaseMouseCapture();
+        }
+
+        private void m_right_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (Keyboard.IsKeyDown(Key.LeftCtrl))
+            {
+                Point relativePoint = m_right.TransformToAncestor(this.Parent as Visual).Transform(new Point(m_right.Width/2, m_right.Height/2));
+                if (evtOnRightDown != null)
+                {
+                    evtOnRightDown(relativePoint);}
+                return;
+            }
+        }
+
+        private void m_left_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (Keyboard.IsKeyDown(Key.LeftCtrl))
+            {
+                Point relativePoint = m_left.TransformToAncestor(this.Parent as Visual).Transform(new Point(m_right.Width / 2, m_right.Height / 2));
+                if(evtOnLeftUp != null)
+                {
+                    evtOnLeftUp(relativePoint);
+                }
+                return;
+            }
         }
     }
 }
