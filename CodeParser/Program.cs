a91bcae666fd5f48ddacea2c417b66360d519d22
xf <lxf0525@gmail.com>
2015-12-01 11:18:15 +0800

    more

---
+++ CodeParser/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var c = new CodeSource();
+            c.setSoruce(
+@"asdf
+1asdf
+1234
+1234asdf
+333388881111
+");
+            c.getchar();
+            c.markPos();
+            c.pushPos();
+
+            for (int i = 0; i < 4; i++ )
+            {
+                c.getchar();
+                c.markPos();
+            }
+            var l = c.getLineMarked();
+                Console.WriteLine(l);
+                c.popPos();
+                c.markPos();
+                l = c.getLineMarked();
+                Console.WriteLine(l);
+        }
+    }
+}
