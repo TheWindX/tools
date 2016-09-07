8b1b84f8151c15f61911019c24e65a5a9d479d04
xf <lxf0525@gmail.com>
2016-09-07 22:12:16 +0800

    TEST is OK

---
+++ minigame/v0829_editor/MiniEditor/components/TEST/COMTest1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniEditor
+{
+    [CustomComponent(path = "TEST", name = "test1", removable = false)]
+    class COMTest1 : MComponent
+    {
+        public bool boolValue { get; set; }
+        public int intValue { get; set; }
+        public double doubleValue { get; set; }
+        public string stringValue { get; set; }
+
+        [Description]
+        public string descripValue { get { return "description from test1"; } }
+
+        public Action act
+        {
+            get
+            {
+                return () =>
+                {
+                    MLogger.info("log from COMTest1");
+                };
+            }
+        }
+    }
+}
---
+++ minigame/v0829_editor/MiniEditor/components/TEST/COMTest2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniEditor
+{
+    [CustomComponent(path = "TEST", name = "test2")]
+    [RequireCom(typeof(COMTest1))]
+    class COMTest2 : MComponent
+    {
+        public bool 布尔值 { get; set; }
+        public int 整型值 {
+            get
+            {
+                return getComponent<COMTest1>().intValue;
+            }
+            set
+            {
+                getComponent<COMTest1>().intValue = value;
+            }
+        }
+        public double 双浮点 { get; set; }
+        public string 字符串 { get; set; }
+
+        [Description]
+        public string descripValue { get { return "description from test2"; } }
+
+        public Action act
+        {
+            get
+            {
+                return () =>
+                {
+                    MLogger.info("log from COMTest2");
+                };
+            }
+        }
+    }
+}
